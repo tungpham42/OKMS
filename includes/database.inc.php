e928dd6b30c6c3781e32f844275ba14eec22190c
Tung Pham <tung.42@gmail.com>
2017-09-15 14:41:21 +0700

    db

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,11 +1,6 @@
 <?php
 require 'libraries/class.database.php';
 /* Database config */
-$url = array(
-	'host' => '127.0.0.1',
-	'user' => 'nhipsinh_okms',
-	'pass' => '0KM$v0d0i',
-	'db' => 'nhipsinh_okms'
-);
-$db = new Database($url["host"],$url["user"],$url["pass"],$url["db"],'OKMS_');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$db = new Database($url["host"],$url["user"],$url["pass"],substr($url["path"], 1),'OKMS_');
 $db->connect();
