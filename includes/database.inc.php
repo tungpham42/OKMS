8ca99e8b3088762b9e264d276b41f21bea913cbc
Tung Pham <tung.42@gmail.com>
2017-09-17 13:27:14 +0700

    DB

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,6 +1,11 @@
 <?php
 require 'libraries/class.database.php';
 /* Database config */
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$db = new Database($url["host"],$url["user"],$url["pass"],substr($url["path"], 1),'OKMS_');
+$url = getenv('JAWSDB_URL');
+$dbparts = parse_url($url);
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+$db = new Database($hostname, $username, $password, $database,'OKMS_');
 $db->connect();
