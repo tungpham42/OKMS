8b0f86644ccc80712af343f73cab0ce1392e6525
Tung Pham <tung.42@gmail.com>
2017-10-26 14:31:42 +0700

    ssl

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -592,12 +592,12 @@ function send_mail($to,$subject,$body,$from) //Send mail with SMTP authenticatio
 	$mail = new PHPMailer;
 	$mail->IsSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
-	$mail->Port = 25;                                     // Set the SMTP port
+	$mail->Port = 465;                                    // Set the SMTP port
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 	$mail->Username = 'okms.vn@gmail.com';                // SMTP username
 	$mail->Password = '0km$v0d0i';                        // SMTP password
-//	$mail->SMTPSecure = 'none';                           // Enable encryption, 'ssl' also accepted
-
+	$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
+	$mail->SMTPDebug = 2;
 	$mail->From = $from;
 	$mail->FromName = 'OKMS';
 	$mail->AddAddress($to);  // Add a recipient
