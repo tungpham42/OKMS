fb8bf29777f56f4f7f7c555195de01ff289f818f
Tung Pham <tung.42@gmail.com>
2017-10-26 12:00:34 +0700

    mail

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -592,11 +592,11 @@ function send_mail($to,$subject,$body,$from) //Send mail with SMTP authenticatio
 	$mail = new PHPMailer;
 	$mail->IsSMTP();                                      // Set mailer to use SMTP
 	$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
-	$mail->Port = 587;                                    // Set the SMTP port
+	$mail->Port = 25;                                     // Set the SMTP port
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
 	$mail->Username = 'okms.vn@gmail.com';                // SMTP username
 	$mail->Password = '0km$v0d0i';                        // SMTP password
-	$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+//	$mail->SMTPSecure = 'none';                           // Enable encryption, 'ssl' also accepted
 
 	$mail->From = $from;
 	$mail->FromName = 'OKMS';
