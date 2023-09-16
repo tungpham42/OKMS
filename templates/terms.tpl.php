6042218ea82dfa5001030f911eea166746e680d2
Tung Pham <tung.42@gmail.com>
2023-09-16 15:33:12 +0700

    Update terms.tpl.php

--- templates/terms.tpl.php
+++ templates/terms.tpl.php
@@ -1,1 +1,14 @@
-This website was made by the Avengers.
+<h2>1. Acceptance of Terms</h2>
+<p>By accessing or using our Knowledge Management System (the "Service"), you agree to comply with and be bound by these Terms and Conditions.</p>
+<h2>2. Use of the Service</h2>
+<p>You agree to use the Service only for lawful purposes and in a way that does not infringe upon the rights of others or inhibit their use and enjoyment of the Service.</p>
+<h2>3. User Accounts</h2>
+<p>You may be required to create an account to access certain features of the Service. You are responsible for maintaining the confidentiality of your account credentials and for any activities that occur under your account.</p>
+<h2>4. Content</h2>
+<p>Users may submit, post, or display content on the Service. By doing so, you grant us a non-exclusive, worldwide, royalty-free license to use, reproduce, modify, and distribute that content within the Service.</p>
+<h2>5. Termination</h2>
+<p>We may terminate or suspend your account and access to the Service at our sole discretion, without notice, for any reason, including violation of these Terms and Conditions.</p>
+<h2>6. Disclaimer</h2>
+<p>The Service is provided "as is" and "as available" without warranties of any kind, either express or implied. We do not guarantee the accuracy or reliability of the content on the Service.</p>
+<h2>7. Limitation of Liability</h2>
+<p>We shall not be liable for any indirect, incidental, special, consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or indirectly, or any loss of data, use, goodwill, or other intangible losses resulting from your use of the Service.</p>
