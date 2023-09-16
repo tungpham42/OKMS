38be774af314e38df6ad18cdcd74e854f8f7475c
Tung Pham <tung.42@gmail.com>
2023-09-16 15:29:37 +0700

    PDO check

--- triggers/email_check.php
+++ triggers/email_check.php
@@ -2,15 +2,14 @@
 require_once '../includes/functions.inc.php';
 require_once '../includes/admin.inc.php';
 $email = (isset($_POST['email'])) ? $_POST['email']: '';
-$rows = mysqli_query($db->link, "SELECT * FROM ".PREFIX."USER WHERE User_Mail='".$email."'");
-if(mysqli_num_rows($rows) == 0)
-{
-	if (check_mail($email)) {
-		echo('EMAIL_AVAILABLE');
-	}
-}
-else
-{
-	echo('EMAIL_EXISTS');
+$emailCheck = $db->link->prepare("SELECT * FROM {$db->db_prefix}USER WHERE User_Mail = :email");
+$emailCheck->bindParam(':email', $email, PDO::PARAM_STR);
+$emailCheck->execute();
+if ($emailCheck->rowCount() == 0) {
+    if (check_mail($email)) {
+        echo 'EMAIL_AVAILABLE';
+    }
+} else {
+    echo 'EMAIL_EXISTS';
 }
 ?>
--- triggers/username_check.php
+++ triggers/username_check.php
@@ -2,13 +2,13 @@
 require_once '../includes/functions.inc.php';
 require_once '../includes/admin.inc.php';
 $username = (isset($_POST['username'])) ? $_POST['username']: '';
-$rows = mysqli_query($db->link, "SELECT * FROM ".PREFIX."USER WHERE User_Username='".$username."'");
-if(mysqli_num_rows($rows) == 0)
-{
-	echo('USERNAME_AVAILABLE');
-}
-else
-{
-	echo('USERNAME_EXISTS');
+$usernameCheck = $db->link->prepare("SELECT * FROM {$db->db_prefix}USER WHERE User_Username = :username");
+$usernameCheck->bindParam(':username', $username, PDO::PARAM_STR);
+$usernameCheck->execute();
+
+if ($usernameCheck->rowCount() == 0) {
+    echo 'USERNAME_AVAILABLE';
+} else {
+    echo 'USERNAME_EXISTS';
 }
 ?>
